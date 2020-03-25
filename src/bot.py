7801a0cbd6038d34779725ad06bd52945f92149d
Robbie <robbiealternate@gmail.com>
2020-03-25 11:18:09 +0000

    BotHelperProcess

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,10 @@
+import pathlib
+
 from rlbot.agents.base_agent import BaseAgent
+from rlbot.botmanager.helper_process_request import HelperProcessRequest
 
 
-class MyBot(BaseAgent):
-    pass
+class Agent(BaseAgent):
+    def get_helper_process_request(self) -> HelperProcessRequest:
+        controller_path = pathlib.Path(__file__).parent.joinpath("controller.py")
+        return HelperProcessRequest(python_file_path=controller_path, key="controller")
--- src/controller.py
+++ src/controller.py
@@ -2,15 +2,18 @@
 
 
 import pygame
+from rlbot.botmanager.bot_helper_process import BotHelperProcess
 
 
-class PS4Controller():
-    def __init__(self):
+class PS4Controller(BotHelperProcess):
+    def __init__(self, agent_metadata_queue, quit_event, options):
+        super().__init__(agent_metadata_queue, quit_event, options)
+
         pygame.init()
         pygame.joystick.init()
         self.controller = pygame.joystick.Joystick(0)
         self.controller.init()
-        
+
         self.axis_data = {}
         self.button_data = {}
         for i in range(self.controller.get_numbuttons()):
@@ -19,11 +22,11 @@ class PS4Controller():
         for i in range(self.controller.get_numhats()):
             self.hat_data[i] = (0, 0)
 
-    def listen(self):
-        while True:
+    def start(self):
+        while not self.quit_event.is_set():
             for event in pygame.event.get():
                 if event.type == pygame.JOYAXISMOTION:
-                    self.axis_data[event.axis] = round(event.value,2)
+                    self.axis_data[event.axis] = round(event.value, 2)
                 elif event.type == pygame.JOYBUTTONDOWN:
                     self.button_data[event.button] = True
                 elif event.type == pygame.JOYBUTTONUP:
@@ -31,7 +34,4 @@ class PS4Controller():
                 elif event.type == pygame.JOYHATMOTION:
                     self.hat_data[event.hat] = event.value
 
-
-if __name__ == "__main__":
-    ps4 = PS4Controller()
-    ps4.listen()
+            print("\n".join([str(item) for item in [self.axis_data, self.button_data, self.hat_data]]))
