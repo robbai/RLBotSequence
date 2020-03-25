3ab66dd8615ca49ccf155f31579d1d172d3c91b8
Robbie <robbiealternate@gmail.com>
2020-03-25 14:39:52 +0000

    Working

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,16 @@
 import pathlib
 
-from rlbot.agents.base_agent import BaseAgent
+from rlbot.agents.base_independent_agent import BaseIndependentAgent
 from rlbot.botmanager.helper_process_request import HelperProcessRequest
 
 
-class Agent(BaseAgent):
+class Agent(BaseIndependentAgent):
     def get_helper_process_request(self) -> HelperProcessRequest:
         controller_path = pathlib.Path(__file__).parent.joinpath("controller.py")
-        return HelperProcessRequest(python_file_path=controller_path, key="controller")
+        options = {"index": self.index}
+        return HelperProcessRequest(
+            python_file_path=controller_path, key=self.name, options=options
+        )
+
+    def run_independently(self, terminate_request_event):
+        pass
--- src/controller.py
+++ src/controller.py
@@ -1,32 +1,45 @@
 # https://gist.github.com/claymcleod/028386b860b75e4f5472
 
 
+from math import copysign
+
 import pygame
 from rlbot.botmanager.bot_helper_process import BotHelperProcess
+from rlbot.utils.structures.game_interface import GameInterface
+from rlbot.utils.structures.bot_input_struct import PlayerInput
+from rlbot.utils.logging_utils import get_logger
+
+
+def deadzone(axis):
+    return copysign(min(abs(axis), 1), axis) if abs(axis) >= 0.1 else 0
 
 
 class PS4Controller(BotHelperProcess):
     def __init__(self, agent_metadata_queue, quit_event, options):
         super().__init__(agent_metadata_queue, quit_event, options)
 
+        # Controls.
         pygame.init()
         pygame.joystick.init()
         self.controller = pygame.joystick.Joystick(0)
         self.controller.init()
+        self.axis_data = [0] * self.controller.get_numaxes()
+        self.button_data = [False] * self.controller.get_numbuttons()
+        self.hat_data = (0, 0) * self.controller.get_numhats()
 
-        self.axis_data = {}
-        self.button_data = {}
-        for i in range(self.controller.get_numbuttons()):
-            self.button_data[i] = False
-        self.hat_data = {}
-        for i in range(self.controller.get_numhats()):
-            self.hat_data[i] = (0, 0)
+        # RLBot.
+        self.index = options["index"]
+        self.game_interface = GameInterface(get_logger(str(self.index)))
 
     def start(self):
+        self.game_interface.load_interface()
+
+        controls = PlayerInput()
+
         while not self.quit_event.is_set():
             for event in pygame.event.get():
                 if event.type == pygame.JOYAXISMOTION:
-                    self.axis_data[event.axis] = round(event.value, 2)
+                    self.axis_data[event.axis] = deadzone(event.value)
                 elif event.type == pygame.JOYBUTTONDOWN:
                     self.button_data[event.button] = True
                 elif event.type == pygame.JOYBUTTONUP:
@@ -34,4 +47,14 @@ class PS4Controller(BotHelperProcess):
                 elif event.type == pygame.JOYHATMOTION:
                     self.hat_data[event.hat] = event.value
 
-            print("\n".join([str(item) for item in [self.axis_data, self.button_data, self.hat_data]]))
+            # Update controls.
+            air_roll = self.button_data[0] or self.button_data[4]
+            controls.throttle = self.button_data[7] - self.button_data[6]
+            controls.steer = self.axis_data[0]
+            controls.pitch = self.axis_data[1]
+            controls.yaw = 0 if air_roll else self.axis_data[0]
+            controls.roll = self.axis_data[0] if air_roll else 0
+            controls.jump = self.button_data[1]
+            controls.boost = self.button_data[2]
+            controls.handbrake = self.button_data[4]
+            self.game_interface.update_player_input(controls, self.index)
